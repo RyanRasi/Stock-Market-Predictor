188c99e8c472f8ad113366fe0efb5e273a1e4ae5
Ryan Maxwell Rasi <theryanrasi@gmail.com>
2021-02-12 17:00:38 +0000

    new main file calls the other files

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#API KEY - 9EIFJBT9JKNJ5BS7
+import retrieveStockData
+
+
+print("Stock Predictor V1\nAuthor - Ryan Rasi\nwww.github.com/ryanrasi\n")
+# API KEY - 9EIFJBT9JKNJ5BS7
+api_key = '9EIFJBT9JKNJ5BS7'
+
+# Apple stock market prices
+stockTicker = "AAPL"
+
+retrieveStockData.retrieveStockData(api_key, stockTicker)
