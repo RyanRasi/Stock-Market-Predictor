f0832312f91ed1594c7fabed2d98ff7f3644dfc2
Ryan Maxwell Rasi <theryanrasi@gmail.com>
2021-02-26 16:57:08 +0000

    retrieved daily adjusted close prices

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
 import retrieveStockData
-import dataAnalysis
-
 
 print("Stock Predictor V1\nAuthor - Ryan Rasi\nwww.github.com/ryanrasi\n")
+
 # API KEY - 9EIFJBT9JKNJ5BS7
 api_key = '9EIFJBT9JKNJ5BS7'
 
 # Asks for stock ticker to analyse
 stockTicker = ""
+
+# Gets user input for stock ticker
 while stockTicker == "":
     stockTicker = input(
-        "Please enter the stock ticker for price history retrieval\n")
+        "Please enter the stock ticker for price history retrieval\n").upper()
     fileName = retrieveStockData.retrieveStockData(api_key, stockTicker)
     break
-
-decision = ""
-while not decision == "1":
-    decision = input(
-        "\nDo you wish to analyse data or exit program? \n1. Analyse data.\n2. Exit Program\n")
-    if decision == "1":
-        dataAnalysis.analyse(fileName)
-        break
-    elif decision == "2":
-        quit()
-        break
--- retrieveStockData.py
+++ retrieveStockData.py
@@ -12,7 +12,7 @@ def retrieveStockData(api_key, stockTicker):
     print("Retrieving stock history up until %s\n" % currentDate)
 
     # JSON file with all the stock market data for AAL from the last 20 years
-    url_string = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=%s&outputsize=full&apikey=%s" % (
+    url_string = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=%s&outputsize=full&apikey=%s" % (
         stockTicker, api_key)
 
     # Save data to this file
@@ -24,16 +24,23 @@ def retrieveStockData(api_key, stockTicker):
             data = json.loads(url.read().decode())
             # Extract stock market data
             data = data['Time Series (Daily)']
-            df = pd.DataFrame(columns=['Date', 'Low', 'High', 'Close', 'Open'])
+            # Set coloumn headers
+            df = pd.DataFrame(
+                columns=['Date', 'Low', 'High', 'Close', 'Adjusted Close', 'Open'])
+
             for k, v in data.items():
                 date = dt.datetime.strptime(k, '%Y-%m-%d')
                 data_row = [date.date(), float(v['3. low']), float(v['2. high']),
-                            float(v['4. close']), float(v['1. open'])]
+                            float(v['4. close']), float(v['5. adjusted close']), float(v['1. open'])]
                 df.loc[-1, :] = data_row
                 df.index = df.index + 1
-        print('Data saved to : %s' % file_to_save)
+            # Reverses order of the rows
+            df = df[::-1]
+        print('Data saved to: %s' % file_to_save)
         df.to_csv(file_to_save)
+        return file_to_save
 
     # If data is up to date already then alert user
     else:
         print('Stock history is already up to date.')
+        return file_to_save
